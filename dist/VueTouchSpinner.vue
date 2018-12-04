235f29b1bfa1efbe7e03509ffc6d87295db94aa1
Nasser <hekmati.nasser@gmail.com>
2018-12-05 00:59:20 +0330

    fixbug package json file

--- dist/VueTouchSpinner.vue
+++ dist/VueTouchSpinner.vue
@@ -23,7 +23,7 @@
 
 <script>
 export default {
-  name: 'VueTouchSpinner',
+  name: 'TouchSpinner',
   data: function() {
     return {
       numericValue: this.value,
--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 import TouchSpinner from './dist/VueTouchSpinner.vue';
 
 export default {
-  install(Vue, options) {
-    Vue.component(TouchSpinner.name, TouchSpinner)
+  install(Vue) {
+    Vue.component('TouchSpinner', TouchSpinner)
   }
 }
+
