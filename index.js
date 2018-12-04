420ef4229a3b827d11e4e84902297a373542573b
Nasser <hekmati.nasser@gmail.com>
2018-12-04 23:26:57 +0330

    add index file

---
+++ index.js
@@ -0,0 +1,9 @@
+import VueTouchSpinner from './dist/VueTouchSpinner.vue';
+
+const VueTouchSpinner {
+  install(Vue, options) {
+    Vue.component(VueTouchSpinner.name, VueTouchSpinner)
+  }
+};
+
+export default VueTouchSpinner;
