3f901cd09322ec68e3f7d6524f9d836787092e64
Nasser <hekmati.nasser@gmail.com>
2018-12-05 13:31:12 +0330

    fix bug pakage json

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -32,7 +32,7 @@ config.plugins = (config.plugins || []).concat([
   new HtmlWebpackPlugin({
     title: 'BookUp',
     filename: 'index.html',
-    template: path.resolve(__dirname, '../exmaple/index.html'),
+    template: path.resolve(__dirname, '../example/index.html'),
   }),
 
   new webpack.optimize.OccurrenceOrderPlugin(),
